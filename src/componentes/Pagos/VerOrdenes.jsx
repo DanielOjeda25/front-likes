0c830017db9930505bf794211ece0517752d6a2e
DanielOjeda25 <ojedadanielalejandro333@gmail.com>
2024-01-15 11:06:57 -0300

    Ordenes listo

--- src/componentes/Pagos/VerOrdenes.jsx
+++ src/componentes/Pagos/VerOrdenes.jsx
@@ -11,8 +11,6 @@ const VerOrdenes = () => {
   };
 
   const formatCreatedAt = (createdAt) => {
-    console.log(createdAt); // "14-01-2024 03:19:19.100"
-
     const [datePart, timePart] = createdAt.split(' ');
     const [day, month, year] = datePart.split('-');
     const [hours, minutes, seconds] = timePart.split(':');
@@ -22,7 +20,6 @@ const VerOrdenes = () => {
 
     return `${formattedDate} - ${formattedTime}`;
   };
-
 
   const padWithZero = (number) => {
     return number.toString().padStart(2, '0');
@@ -39,21 +36,26 @@ const VerOrdenes = () => {
   };
 
   return (
-    <div className="max-w-md mx-auto mt-10 p-4 border border-gray-300 rounded-md">
-      <h2 className="text-xl font-semibold mb-4">Order consultation</h2>
-      <form>
-        <label className="block text-sm font-medium text-gray-700">Email:</label>
-        <input
-          type="email"
-          value={email}
-          onChange={handleEmailChange}
-          placeholder="Enter your email"
-          className="mt-1 p-2 border border-gray-300 rounded w-full text-sm focus:outline-none focus:border-blue-500"
-        />
+    <div className="md:w-1/2 mx-auto mt-10 p-4 border border-gray-300 rounded-md ">
+      <h2 className="text-xl font-semibold mb-4">Order Consultation</h2>
+      <form className="space-y-4 ">
+        <div className="flex flex-col items-center w-full">
+          <label htmlFor="email" className="block text-sm font-medium text-left text-gray-700 w-full">
+            Email:
+          </label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+            placeholder="Enter your email"
+            className="mt-1 p-2 border border-gray-300 rounded w-full text-sm focus:outline-none focus:border-blue-500"
+          />
+        </div>
         <button
           type="button"
           onClick={handleConsultarClick}
-          className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-md focus:outline-none"
+          className="bg-blue-500 text-white px-4 py-2 rounded-md focus:outline-none"
         >
           Consult
         </button>
@@ -61,13 +63,25 @@ const VerOrdenes = () => {
 
       {result != null && (
         <div className="mt-4">
-          <h3 className="text-lg font-semibold mb-2">Purchase information</h3>
-          <p>Email: {result[0]?.email}</p>
-          <p>Order Id: {result[0]?.orderId}</p>
-          <p>Quantity: {result[0]?.quantity}</p>
-          <p>Price: {result[0]?.price}</p>
-          <p>Status: {result[0]?.statusPayment}</p>
-          <p>Created: {formatCreatedAt(result[0]?.created_at)}</p>
+          <h3 className="text-lg font-semibold mb-2">Purchase Information</h3>
+          <p>
+            <span className="font-semibold">Email:</span> {result[0]?.email}
+          </p>
+          <p>
+            <span className="font-semibold">Order Id:</span> {result[0]?.orderId}
+          </p>
+          <p>
+            <span className="font-semibold">Quantity:</span> {result[0]?.quantity}
+          </p>
+          <p>
+            <span className="font-semibold">Price:</span> {result[0]?.price}
+          </p>
+          <p>
+            <span className="font-semibold">Status:</span> {result[0]?.statusPayment}
+          </p>
+          <p>
+            <span className="font-semibold">Created:</span> {formatCreatedAt(result[0]?.created_at)}
+          </p>
         </div>
       )}
     </div>
